c1626636c3e1aefd16da1122b4533b5160333500
Ashwin <ashwinkumar17020@cse.ssn.edu.in>
2020-06-05 12:18:35 +0530

    Commit

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -3,10 +3,6 @@ import axios from "axios";
 import Navs from './Nav';
 import Display from './DisplayExercise';
 import { Label, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Input } from 'reactstrap';
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
-
 
 class Exercise extends Component {
   constructor(props) {
@@ -65,10 +61,6 @@ class Exercise extends Component {
     this.setState({ dropDownValueextype: e.currentTarget.textContent }, () => {
       this.filter();
     });
-
-
-
-
 
   }
 
@@ -223,66 +215,7 @@ class Exercise extends Component {
         </div>
       </div>
     );
-  }
-        });
-    }
-
-    changeValueextype(e) {
-  		this.setState({dropDownValueextype: e.currentTarget.textContent})
-	  }
-
-	changeValueexpriority(e) {
-		this.setState({dropDownValueexpriority: e.currentTarget.textContent})
-    }
-
-    render() {
-            console.log(this.props.match.params.userId);
-            return (
-            <div>
-            <Navs userId={localStorage.getItem("userId")}/>       
-                <div id="search">            	             
-                  <b><Label htmlFor="category" className="exlabel">CATEGORY</Label></b>
-                  
-                  <ButtonDropdown isOpen={this.state.dropdownOpentype} toggle={this.toggledroptype} id="exdroptype">
-                  <DropdownToggle color="warning" toggle={this.toggledroptype} caret style={{fontSize: '19px'}}>{this.state.dropDownValueextype}</DropdownToggle>
-                  <DropdownMenu className="drop">
-                    <DropdownItem className="typelist" onClick={this.changeValueextype} >Home</DropdownItem>
-                    <hr/>
-                    <DropdownItem className="typelist" onClick={this.changeValueextype} >Work</DropdownItem>
-                    <hr/>
-                    <DropdownItem className="typelist" onClick={this.changeValueextype} >Shopping</DropdownItem>
-                    <hr/>
-                    <DropdownItem className="typelist" onClick={this.changeValueextype} >Edu</DropdownItem>
-                    <hr/>
-                    <DropdownItem className="typelist" onClick={this.changeValueextype} >Payments</DropdownItem>
-                    <hr/>
-                    <DropdownItem className="typelist" onClick={this.changeValueextype} >Misc</DropdownItem>
-                  </DropdownMenu>
-                  </ButtonDropdown>
-
-                  <b><Label htmlFor="category" className="exlabel" style={{marginLeft: '20px'}} >DUE DATE</Label></b>
-
-                  <Input type="date" className="input" id="exdue" name="exdue" max="2030-01-01" innerRef={(input) => this.exdue = input}/>
-
-                  <b><Label htmlFor="priority" className="exlabel" style={{marginLeft: '20px'}} >PRIORITY</Label></b>
-
-                  <ButtonDropdown isOpen={this.state.dropdownOpenpriority} toggle={this.toggledroppriority} id="exdroppriority">
-                    <DropdownToggle color="warning" toggle={this.toggledroppriority} caret style={{fontSize: '19px'}} >{this.state.dropDownValueexpriority}</DropdownToggle>
-                    <DropdownMenu className="drop">
-                      <DropdownItem className="typelist" onClick={this.changeValueexpriority} >High</DropdownItem>
-                      <hr/>
-                      <DropdownItem className="typelist" onClick={this.changeValueexpriority} >Medium</DropdownItem>
-                      <hr/>
-                      <DropdownItem className="typelist" onClick={this.changeValueexpriority} >Low</DropdownItem>
-                    </DropdownMenu>
-                  </ButtonDropdown> 
-                  </div>
-                  <div>   
-                <Display exercise = {this.state.exercises}/>
-                </div>
-            </div>
-            );
-    }
+  }    
 }
 
 
